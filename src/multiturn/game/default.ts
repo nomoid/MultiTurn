a1ace86931a60a5d74b8df9458ca698057af5649
Markus Feng <markusfeng@gmail.com>
2019-02-28 09:39:20 -0500

    Added default state mask

--- src/multiturn/game/default.ts
+++ src/multiturn/game/default.ts
@@ -4,6 +4,7 @@ import SIOServerNetworkLayer from '../sio-network/server/layer';
 import { SIOServer } from '../sio-network/sio-external';
 import UniversalStateManager from '../state/universal';
 import { ServerSyncLayer } from '../sync/server';
+import Player from './player';
 
 export function defaultSyncLayer(io: SIOServer): ServerSyncLayer {
   const netLayer = new SIOServerNetworkLayer(io);
@@ -11,4 +12,11 @@ export function defaultSyncLayer(io: SIOServer): ServerSyncLayer {
   const stateManager = new UniversalStateManager('');
   const syncLayer = new RepeatServerSyncLayer(authLayer, stateManager);
   return syncLayer;
+}
+
+export function defaultStateMask<R, T>():
+    (state: T, player: Player<R>) => string {
+  return (state, _) => {
+    return JSON.stringify(state);
+  };
 }
