857e729e6a7432a9b980fdc89e7170cc2c95e1c9
Markus Feng <markusfeng@gmail.com>
2019-03-06 12:45:25 -0500

    Updated client to use new server framework

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,37 +1,12 @@
 import * as sio from 'socket.io-client';
-import { defaultClientSyncLayer } from '../multiturn/game/client';
-import { ClientSyncResponder, ClientSyncStateEvent, ClientSyncRequestEvent } from '../multiturn/sync/client';
+import { defaultClientSyncLayer, ClientGameResponder, Client } from '../multiturn/game/client';
+import Remote from '../server/tictactoe-new/remote';
 
 const io = sio();
 
-class TestResponder implements ClientSyncResponder {
-
-  private localState: number = 0;
-
-  public onUpdateState(e: ClientSyncStateEvent): void {
-    console.log(`State updated, new state: ${e.state}`);
-    this.localState = parseInt(e.state, 10);
-  }
-
-  public onRequest(e: ClientSyncRequestEvent): Promise<string> {
-    console.log(`Request: ${e.key}, ${e.message}`);
-    if (e.key === 'response') {
-      if (e.message === 'double') {
-        this.localState *= 2;
-        return Promise.resolve(this.localState.toString());
-      }
-      else if (e.message === 'increment') {
-        this.localState += 1;
-        return Promise.resolve(this.localState.toString());
-      }
-      throw new Error(`Request ${e.message} not found`);
-    }
-    throw new Error(`Key ${e.key} not found`);
-  }
-}
-
 function main() {
-  const layer = defaultClientSyncLayer(io, new TestResponder());
+  const layer = defaultClientSyncLayer(io, new ClientGameResponder(
+    new Remote()));
   layer.listen();
 }
 
--- src/server/tictactoe-new/remote.ts
+++ src/server/tictactoe-new/remote.ts
@@ -1,19 +1,43 @@
+import { Client } from '../../multiturn/game/client';
 import { remote } from '../../multiturn/remote/remote';
+import { ClientSyncStateEvent } from '../../multiturn/sync/client';
+import Board from './board';
 import Move from './move';
 
-export default class Remote {
+export default class Remote implements Client<Remote> {
+  private playerNum!: number;
+  private state!: Board;
 
   @remote(Move)
   public getMove(): Promise<Move> {
-    return Promise.resolve(new Move(1, 1));
-  }
-
-  @remote(Move)
-  public getDelayedMove(): Promise<Move> {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        resolve(new Move(1, 0));
+        const move = this.getRandomMove();
+        resolve(move);
       }, 1000);
     });
+  }
+
+  // Client methods
+  public assignNumber(num: number) {
+    this.playerNum = num;
+  }
+
+  public updateState(e: ClientSyncStateEvent) {
+    this.state = JSON.parse(e.state) as Board;
+  }
+
+  public getRemote(): Remote {
+    return this;
+  }
+
+  public gameOver(message: string) {
+    console.log(`Game over! ${message}`);
+  }
+
+  private getRandomMove(): Move {
+    const randomX = Math.floor(Math.random() * 3);
+    const randomY = Math.floor(Math.random() * 3);
+    return new Move(randomX, randomY);
   }
 }
