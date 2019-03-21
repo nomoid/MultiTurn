99c0e88149a6e450c498f530d30ffd7edb0dd75e
Markus Feng <markusfeng@gmail.com>
2019-03-21 23:01:49 +0900

    Added checkmate checking

--- src/chess/game.ts
+++ src/chess/game.ts
@@ -2,7 +2,7 @@ import Server from '../multiturn/game/server';
 import Board from './board';
 import Move from './move';
 import Remote from './remote-dev';
-import { Color, Coordinate, coordToString, numToColor } from './rules';
+import { Color, Coordinate, coordToString, numToColor, opponent } from './rules';
 
 import * as log from 'loglevel';
 
@@ -14,9 +14,27 @@ function end(move: Move): Coordinate {
   return [move.endFile, move.endRank];
 }
 
+function colorToNum(color: Color): number {
+  if (color === 'white') {
+    return 1;
+  }
+  else {
+    return 2;
+  }
+}
+
 export default function getRunner(board: Board) {
   return async (game: Server<Remote, Board>) => {
     const player = game.getCurrentPlayer();
+    const color = numToColor(player.num);
+    if (board.getAllValidMoves(color).length === 0) {
+      if (board.isInCheck(color)) {
+        game.gameOver(colorToNum(opponent(numToColor(player.num))).toString());
+      }
+      else {
+        game.gameOver((0).toString());
+      }
+    }
     const validator = (possibleMove: Move) =>
       board.tryMove(numToColor(player.num),
         start(possibleMove),
